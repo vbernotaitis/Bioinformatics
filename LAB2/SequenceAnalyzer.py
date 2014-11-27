2b63edbff0a194f5affb7f5fc7549255e5852793
Vilimantas Bernotaitis <v.bernotaitis@gmail.com>
2014-11-27 23:32:20 +0200

    Updated SequenceAnalyzer.py

--- LAB2/SequenceAnalyzer.py
+++ LAB2/SequenceAnalyzer.py
@@ -1,4 +1,4 @@
-__author__ = 'Vilius'
+__author__ = 'Vilimantas Bernotaitis'
 
 import os
 from Bio.Blast import NCBIWWW
@@ -6,9 +6,8 @@ from Bio.Blast import NCBIXML
 from Bio import SeqIO
 
 
-class Analyzer:
-    def __init__(self, protein_path, search_output_file, blasta_file, mafft_output, program, database, entrez_query,
-                 query_coverage):
+class SequenceAnalyzer:
+    def __init__(self, protein_path, search_output_file, blasta_file, mafft_output, program, database, entrez_query, query_coverage):
         self.protein_path = protein_path
         self.record = self.load_protein()
         self.program = program
@@ -24,30 +23,22 @@ class Analyzer:
 
     def blast(self):
         blast_records = self.get_blast_records()
-        query_letters = blast_records.query_letters
-        not_valid_alignments = []
 
         for alignment in blast_records.alignments:
             align_length = 0
             for hsp in alignment.hsps:
                 align_length += hsp.align_length
-            coverage = align_length / query_letters * 100
+            coverage = align_length / blast_records.query_letters * 100
             if coverage < self.queryCoverage:
-                not_valid_alignments.append(alignment)
-
-        for alignment in not_valid_alignments:
-            blast_records.alignments.remove(alignment)
+                blast_records.alignments.remove(alignment)
 
         self.save_blast_record(blast_records)
 
     def get_blast_records(self):
         if not os.path.isfile(self.search_output_file):
-            blast_output = NCBIWWW.qblast(self.program, self.database, self.record.seq,
-                                          entrez_query=self.entrez_query, hitlist_size=500, expect=100.0)
-
-            output_file = open(self.search_output_file, "w")
-            output_file.write(blast_output.read())
-            output_file.close()
+            blast_output = NCBIWWW.qblast(self.program, self.database, self.record.seq, self.entrez_query, 500, 100.0)
+            with open(self.search_output_file, "w") as tempFile:
+                tempFile.write(blast_output.read())
 
         blast_file = open(self.search_output_file)
         return NCBIXML.read(blast_file)
@@ -58,20 +49,23 @@ class Analyzer:
             for hsp in alignment.hsps:
                 fasta_list.append(">{0}\n{1}\n\n".format(alignment.title, hsp.sbjct))
 
-        with open(self.blasta_file, "w") as tempFile:
-            tempFile.writelines(fasta_list)
+        #with open(self.blasta_file, "w") as tempFile:
+        #   tempFile.writelines(fasta_list)
 
-    def mafft(self, output="mafft_output.fasta"):
-        os.system("mafft\mafft --localpair --quiet {0} > {1}".format(self.blasta_file, output))
+    def mafft(self):
+        os.system("mafft\mafft --localpair --quiet {0} > {1}".format(self.blasta_file, self.mafft_output))
         return
 
     def analyze(self, frag_len):
+        self.blast()
+        self.mafft()
+
         all_seqs = []
         for r in SeqIO.parse(open(self.mafft_output), format="fasta"):
             all_seqs.append(r.seq)
 
         main_sequence = all_seqs[0]
-        min_differences = 0
+        min_differences = len(main_sequence) * len(all_seqs)-1
         min_index = 0
         max_differences = 0
         max_index = 0
@@ -79,15 +73,24 @@ class Analyzer:
         for i in range(0, len(main_sequence) - frag_len):
             differences = self.differences_in_fragment(main_sequence, all_seqs, i, frag_len)
 
-            if differences > max_differences:
-                min_differences = differences
-                min_index = i
             if differences < min_differences:
                 min_differences = differences
+                min_index = i
+            if differences > max_differences:
+                max_differences = differences
                 max_index = i
 
-        print("Mažiausiai panaši seka: {0}".format(main_sequence[min_index:(min_index + frag_len)]))
-        print("Labiausiai panaši seka: {0}".format(main_sequence[max_index:(max_index + frag_len)]))
+        print("Originaliausia seka: {0}".format(main_sequence[max_index:(max_index + frag_len)]))
+        print("Index: {0}".format(max_index))
+        print("Skirtumai: {0}".format(max_differences))
+        for i in range(0, len(all_seqs)):
+            print(all_seqs[i][max_index:max_index+frag_len])
+
+        print("Labiausiai panaši seka: {0}".format(main_sequence[min_index:(min_index + frag_len)]))
+        print("Index: {0}".format(min_index))
+        print("Skirtumai: {0}".format(min_differences))
+        for i in range(0, len(all_seqs)):
+            print(all_seqs[i][min_index:min_index+frag_len])
         return
 
     @staticmethod
@@ -103,16 +106,15 @@ class Analyzer:
 
 
 def main():
-    analyzer = Analyzer("data\\serum_albumin_preproprotein.fasta",
+    analyzer = SequenceAnalyzer("data\\serum_albumin_preproprotein.fasta",
                         "data\\search_output.xml",
                         "data\\blast_output.fasta",
-                        "data\\mafft_output.fasta"
+                        "data\\mafft_output.fasta",
                         "blastp",
                         "swissprot",
                         '"serum albumin"[Protein name] AND mammals[Organism]',
                         80)
-    analyzer.blast()
-    analyzer.mafft()
+
     analyzer.analyze(15)
 
 
